b9c8d967e12160b58f5b695592466583484a0705
denis <vgdenisomar@gmail.com>
2019-07-28 22:11:16 -0600

    cambios

--- obtfrontend/src/Components/Pages/Private/products/products.js
+++ obtfrontend/src/Components/Pages/Private/products/products.js
@@ -1,13 +1,65 @@
 import React, { Component } from 'react';
 import './products.css'
+import {Link} from 'react-router-dom';
+import { IoIosAdd, IoIosSync, IoMdAddCircle } from 'react-icons/io';
+import { paxios } from '../../../../Utilities';
 /*
   module.exports = class Login .....
 */
 export default class Login extends Component{
+
+  constructor(){
+    super();
+    this.state={
+      things:[],
+      hasMore:true,
+      page:1,
+      intervalIsSet: false,
+      itemsToLoad:10
+    }}
+  componentDidMount() {
+    this.getDataFromDb();
+    if (!this.state.intervalIsSet) {
+      let interval = setInterval(this.getDataFromDb, 2000);
+      this.setState({ intervalIsSet: interval });
+    }
+  }
+
+  componentWillUnmount() {
+    if (this.state.intervalIsSet) {
+      clearInterval(this.state.intervalIsSet);
+      this.setState({ intervalIsSet: null });
+    }
+  }
+  getDataFromDb = () => {
+    const uri = `/api/things`;
+    paxios.get(uri)
+    .then(
+      ({data})=>{
+        console.log(data);
+        this.setState(
+          {
+            things:data
+          }
+        )
+      })
+  };
+
     render(){
+      const { things } = this.state;
         return (
           <section>
             <h1>Sustento</h1>
+            {things.length <= 0
+            ? 'NO DB ENTRIES YET'
+            : things.map((dat) => (
+                <div className="thingItem" key={dat._id}>
+                  <span> {dat.descripcion}</span>
+                  <Link to={`/detail/${dat._id}`}>
+            <IoIosAdd size="2em"/>
+          </Link>
+                </div>
+              ))}
           </section>
         );
 }
